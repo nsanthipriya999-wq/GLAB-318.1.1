ab8c2213268492c1bf75d2103deefa3fcdc45614
santhi999 <nsanthipriya999@gmail.com>
2026-06-12 23:15:09 -0400

    final

--- index.js
+++ index.js
@@ -8,20 +8,29 @@ const server = http.createServer((req, res) => {
   switch(req.url)
   {
       case "/":
-        res.write("Hello World");
+        res.statusCode=200;
+        res.write('<h1 style="color: red">Hello World!</h1>');
+        res.write('<p>I wonder what else we can send...</p>');
         break;
       case "/test":
-        res.write("Test");
+        res.write(`<h1>My Test page</h1>`);
         break;
       case  "/cat":
-        res.write("http://cat.com/cat1");
+        res.write(`<h1 style="color: blue"> Hi,I'm Timmy!</h1>`);
+        res.write('<p>Welcome to my Home Page</p>');
+        res.write(`<a href="http://cat.com/cat1">My web page</a>`);
+
         break;
       case "/dog":
-        res.write("http://dog.com/dog1");
+        res.write(`<h1 style="color:brown"> Hi,I'm Tommy!</h1>`);
+        res.write('<p>Welcome to my Home Page</p>');
+        res.write(`<a href="http://dog.com/dog1">My web page</a>`);
         break;
       default:
         res.statusCode=404;
-        res.write("Error: 404 Not Found");
+        res.write(`<h1>Error: 404 Not Found</h1>`);
+        res.write(`<p>That route doesn't exist</p>`);
+
         break;
   }
   res.end();
